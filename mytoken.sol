1c4ad61684f91e868e25e88f6affe0cc5969f037
anandkumarmotion <anandkumarmotion@gmail.com>
2024-06-29 22:18:18 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,21 +1,24 @@
-// Public variables here
+pragma solidity 0.8.18;
+contract MyToken {
 
-string public tokenName = "MyethToken"; // Token Name
-string public tokenAbbreviation = "METK"; // Token Abbreviation
-uint totalSupply= 0; // Total Supply
+    // Public variables here
+    string public tokenName = "MyethToken"; // Token Name
+    string public tokenAbbreviation = "METK"; // Token Abbreviation
+    uint totalSupply= 0; // Total Supply
 
-// Mapping variable here
-mapping(address => uint) public balances;
+    // Mapping variable here
+    mapping(address => uint) public balances;
 
-// Mint function
-function mint(address to, uint _value) public {
-    totalSupply += value;
-    balances[to] += value;
-}
+    // Mint function
+    function mint(address to, uint _value) public {
+        totalSupply += value;
+        balances[to] += value;
+    }
 
-// Burn function
-function burn(address from, uint _value) public {
-    require(balances[from] >= value, "Insufficient balance to burn");
-    totalSupply -= value;
-    balances[from] -= value;
+    // Burn function
+    function burn(address from, uint _value) public {
+        require(balances[from] >= value, "Insufficient balance to burn");
+        totalSupply -= value;
+        balances[from] -= value;
+    }
 }
